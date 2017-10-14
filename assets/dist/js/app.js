e494f05d2734b368bb851dde6f086b38dde44905
samueleyre <samuel.eyre@hotmail.fr>
2017-10-14 17:37:40 +0200

    small visual update

--- assets/dist/js/app.js
+++ assets/dist/js/app.js
@@ -327,6 +327,7 @@ jQuery(document).ready(function($) {
                 "<hr>"+
                 "<div class=\"project_description\">"+
                   "<p>"+values[i][1]+"</p>"+
+                  "<div class=\"hideLongDescription\"></div>"+
                 "</div>"+
               "</div>"+
               "<div class=\"center\">"+
